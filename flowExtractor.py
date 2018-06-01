e5b2821ebde7afdb7d0dfeb5929fb75a6715ab75
Karel Durkota <karel.durkota@gmail.com>
2018-06-01 18:40:41 +0200

    Updated the file, so that it can be used in jupyter as well.

--- flowExtractor.py
+++ flowExtractor.py
@@ -6,6 +6,9 @@ import argparse
 import csv
 import re
 from collections import defaultdict
+import pandas as pd
+import numpy as np
+
 
 __version__ = '0.1'
 # ['StartTime','Dur','Proto','SrcAddr','Sport','Dir','DstAddr','Dport','State','sTos','dTos','TotPkts','TotBytes','SrcBytes','SrcPkts','Label','srcUdata','dstUdata']
@@ -16,7 +19,6 @@ def getHeader(filename):
     with open(filename, "r") as csvfile:
         return csvfile.readline().split(',')
     print(col)
-
 
 def getProductionIp(port, filename):
 
@@ -31,12 +33,17 @@ def getProductionIp(port, filename):
         if (args.debug > 0):
             print('Production IP = {0}'.format(production_ip[0]))
         return production_ip[0]
-def printFiltered(ip, port, filename):
+
+def getFiltered(ip, port, filename):
     global col
+
+    data = []
     with open(filename, "r") as csvfile:
         csvfile.readline() # read the header
 
         reader = csv.reader(csvfile, delimiter=',')
+        #for row in reader:
+        #    print(len(row))
 
         filtered = filter(lambda p:
                                str(port) == p[col['Dport']] and
@@ -46,35 +53,38 @@ def printFiltered(ip, port, filename):
                                reader)
 
         for row in filtered:
-            print(','.join(row))
+            data.append(row[:len(col)])
+    return data
 
+def extract(argv):
+    global args
 
-def main(args):
-    global col
-    
-    header = getHeader(args.filename)
-    col = dict([(header[i], i) for i in range(len(header))])
-
-    if ( args.ip is None ):
-        production_ip = getProductionIp(args.port, args.filename)
-    else:
-        production_ip = args.ip
-
-    print(','.join(header), end='')
-
-    printFiltered(production_ip, args.port, args.filename)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Program filters only the flows that have: i) given IP address in the Destination; ii) given port in the destPort; iii) contains S in the Source state.",
-                                     usage='%(prog)s --ip <ip_address> --port <port>')
+    parser = argparse.ArgumentParser(description="Program filters only the flows that have: i) given IP address in the Destination; ii) given port in the destPort; iii) contains S in the Source state.", usage='%(prog)s --ip <ip_address> --port <port>')
     parser.add_argument('-i', '--ip', help='The destination IP address. If omitted, script searches for IP address that occures in all lines. The header line is assumed to contain DstAddr attribute.', action='store', required=False)
     parser.add_argument('-p', '--port', help='The destination port. The header line is assumed to contain Dport attribute.', action='store', required=True, type=int)
     parser.add_argument('-f', '--filename', help='The CSV file with netflows. The first line is assumed to be header.', action='store', required=argparse.FileType('r'))
     parser.add_argument('-d', '--debug', help='Amount of debugging. This shows inner information about the run.', action='store', default=0, required=False, type=int)
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
-    # example call
-    # args = parser.parse_args(['-i','147.32.83.179', '-p','80','-f','./omnia1/Traffic/2017-07-22-147.32.83.179.binetflow', '-d', '1'])
 
-    main(args)
+    global col
+    header = getHeader(args.filename)
+    # ['StartTime','Dur','Proto','SrcAddr','Sport','Dir','DstAddr','Dport','State','sTos','dTos','TotPkts','TotBytes','SrcBytes','SrcPkts','Label','srcUdata','dstUdata']
+    col = dict([(header[i], i) for i in range(len(header))])
+
+    if ( args.ip is None ):
+        production_ip = getProductionIp(args.port, args.filename)
+        print('Production IP is {0}'.format(production_ip))
+    else:
+        production_ip = args.ip
+
+#    output[0] = header
+
+    # print(getFiltered(production_ip, args.port, args.filename))
+    data = getFiltered(production_ip, args.port, args.filename)
+    data.insert(0, header)
+    return data
+
+if __name__ == "__main__":
+    data = extract(sys.argv[1:])
+    print(data)
